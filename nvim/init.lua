34d11f8165fe9f04cfdaadca986ecf09578bc400
AF <af.arthurfaria@gmail.com>
2025-01-13 23:12:45 -0300

    basic neovim configuration - initial commit

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+require("config.lazy")
+require("config.telescope")
+require("config.treesitter")
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("colorscheme kanagawa-wave")
+vim.g.mapleader = " "
---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,4 @@
+return {
+  {"rebelot/kanagawa.nvim", name = "kanagawa", priority = 2000 },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+}
